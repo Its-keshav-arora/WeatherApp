a95099edff5b63326b86a851d1f6a826a480b126
Keshav Arora <keshav11y@gmail.com>
2024-06-13 15:03:42 +0530

    npm build and .env added

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import SearchBox from "./components/SearchBox"
 
 function App() {
 
-  const API_KEY = import.meta.env.VITE_APP_API_KEY;
-  console.log(import.meta.env);
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  console.log(process.env.REACT_APP_API_KEY);
 
   return (
     <div>
         <h1 className="text-center text-4xl mt-6"><span className=" bg-gradient-to-r bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text font-bold    text-transparent ">Weather App</span></h1>
-          <SearchBox apikey= {API_KEY}> </SearchBox>
+          <SearchBox apikey={API_KEY}> </SearchBox>
     </div>
   )
 }
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -12,8 +12,8 @@ import WeatherCard from "./weatherCard";
 // importing the Error Page
 import Error from "./Error";
 
-export default function SearchBox({apikey}) {
-  console.log(apikey);
+export default function SearchBox({ apikey }) {
+  // console.log(apikey);
   const [city, setCity] = useState("");
   const [CitySearched, setCitySearched] = useState(false);
   const [cityNotFound, setCityNotFound] = useState(false);
@@ -38,7 +38,7 @@ export default function SearchBox({apikey}) {
   });
 
   const URL = "http://api.openweathermap.org/geo/1.0/direct"; // URL of the geoCoordinates API
-  const API_KEY =  apikey;// API key of geoCoordinates API
+  const API_KEY = apikey;// API key of geoCoordinates API
 
   const WeatherURL = "https://api.openweathermap.org/data/2.5/"; // URL of the Weather API
 
